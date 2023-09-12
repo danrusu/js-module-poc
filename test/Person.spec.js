dec861bc982f305b3c5ad8aa8b6066301f4e998d
Dan Rusu <dangingko@yahoo.com>
2023-09-12 09:58:14 +0300

    import.meta

--- test/Person.spec.js
+++ test/Person.spec.js
@@ -1,7 +1,11 @@
+import fs from 'node:fs/promises';
+import { expect } from 'chai';
+
 import Person, { isPerson } from '../src/Person.js';
 import * as Person1 from '../src/Person.js';
+import exp from 'node:constants';
 
-describe('Person test suite', () => {
+describe('Module test suite', () => {
   it('test static module import', () => {
     const person = new Person('John', 42);
     console.log(person.toString());
@@ -15,5 +19,18 @@ describe('Person test suite', () => {
     console.log(`isPerson: ${Person2.isPerson}`);
 
     console.log(Person1 === Person2);
+    expect(Person1).equals(Person2);
+
+    console.log(`Module prototype: ${Object.getPrototypeOf(Person2)}`); // prototype is null
+    expect(Object.getPrototypeOf(Person2)).to.be.null;
+  });
+
+  it('test import.meta', async () => {
+    console.log(import.meta);
+
+    // ES module: use URLs rather than filesystem paths for working with files
+    const fileURL = new URL('../resources/message.txt', import.meta.url);
+    const message = await fs.readFile(fileURL, 'utf8');
+    expect(message).equals('Hello world!');
   });
 });
